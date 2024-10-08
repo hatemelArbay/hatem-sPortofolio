1de0577dd5387f6abee158c1b7c639e775ccc620
hatemelArbay <hatemel3arby@gmail.com>
2024-10-08 19:23:41 +0300

    update project dispaly on mobile view

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ const ProjectCard = ({
       variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
       className={`relative ${
         active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-      } flex items-center justify-center min-w-[170px] h-[600px] cursor-pointer card-shadow`}
+      } flex items-center justify-center min-w-[170px] h-[400px] cursor-pointer card-shadow lg:h-[600px]`}
       onClick={() => handleClick(id)}
     >
       <div className="absolute top-0 left-0 z-10 bg-jetLight h-full w-full opacity-[0.5] rounded-[24px]"></div>
